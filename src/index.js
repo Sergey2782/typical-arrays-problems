bc45fa9a38660eec196d38be3d241bb72e281da3
Sergey2782 <msergun234@gmail.com>
2020-03-23 13:05:44 +0300

    feat: add typ-arr-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(arguments.length == 0 || array.length == 0){
+        return 0;
+    }
+    if(Array.isArray(array))
+      array.sort(function(a, b) {
+          return b - a;
+});
+      return array[(array.length-1)];
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+    if(arguments.length == 0 || array.length == 0){
+        return 0;
+    }
+      if(Array.isArray(array))
+        array.sort(function(a, b) {
+            return a - b;
+  });
+        return array[(array.length-1)];
+  }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(arguments.length == 0 || array.length == 0){
+        return 0;
+    }
+   if(Array.isArray(array))
+        return array.reduce((a, b) => (a + b)) / array.length;
+
 }
